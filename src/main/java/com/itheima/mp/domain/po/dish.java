c3101bacaa0bb6ce67563516ac2488d3b8e0b444
ysnuibi <2332763576@qq.com>
2025-04-10 16:47:28 +0800

    新增dish

---
+++ src/main/java/com/itheima/mp/domain/po/dish.java
@@ -0,0 +1,9 @@
+package com.itheima.mp.domain.po;
+
+public class dish {
+    private Integer id;
+    private String name;
+    private String type;
+    private String price;
+    private String img;
+}
